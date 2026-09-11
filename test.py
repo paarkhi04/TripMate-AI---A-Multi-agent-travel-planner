797c752f5d7978b39fb5575a1d3378738121ec17
Paarkhi Tyagi <paarkhityagi04@gmail.com>
2026-09-12 01:51:01 +0530

    frontend

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
-res = search_flights("Plan a 7 days Japan trip from Bangladesh")
-print(res)
+user_iput = input("enter travel request:")
+response = run_travel_agent(
+    user_input = user_iput,
+    thread_id ="test_user"
+)
+
+print("\nFINAL ANSWER:\n")
+
+print(response["answer"])
+
+#res = search_flights("Plan a 7 days Japan trip from Bangladesh")
+# print(res)
 
 #res = tavily_search("best hotels in india")
 #print(res)
